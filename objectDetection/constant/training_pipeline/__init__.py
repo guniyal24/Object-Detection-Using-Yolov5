a020275756d4c3afdbe5fe7a0ffecb66251f6b71
guniyl24 <guniyal3202@gmail.com>
2025-01-15 21:16:15 +0530

    data validation added

--- objectDetection/constant/training_pipeline/__init__.py
+++ objectDetection/constant/training_pipeline/__init__.py
@@ -5,3 +5,10 @@ DATA_INGESTION_DIR_NAME : str = 'data_ingestion'
 DATA_INGESTION_FEATURE_STORE_DIR : str = 'feature_store'
 
 DATA_DOWNLOAD_URL : str = 'https://drive.google.com/file/d/1DFge4QDRAJJkWEmvwwN4CrrJ2U0l5Nd_/view?usp=drive_link'
+
+
+DATA_VALIDATION_DIR_NAME : str = 'data_validation'
+
+DATA_VALIDATION_STATUS_FILE : str =  'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train" , "valid" , "data.yaml"]
